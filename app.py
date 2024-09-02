5dafce336666aa79a5c22d7742a761faa9dd135b
RaunakSinghSoi <144322027+RaunakSinghSoi@users.noreply.github.com>
2024-09-01 20:35:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import pandas as pd
 import os
 from werkzeug.utils import secure_filename
